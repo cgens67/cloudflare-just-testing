b35eb232bc9b8f884cd3a4ac8e1af556079b565b
cgens67 <cgenshin587@gmail.com>
2026-04-23 17:13:51 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,16 +6,11 @@ import { AudioPlayer } from "@/components/audio-player"
 export default function Page() {
   const [hasMounted, setHasMounted] = useState(false)
 
-  // This hook only runs in the browser. 
-  // It prevents the Player from ever trying to render on Cloudflare's server.
   useEffect(() => {
     setHasMounted(true)
-  }, [])
+  },[])
 
-  if (!hasMounted) {
-    // Show a black screen while the browser loads
-    return <div className="h-screen w-screen bg-black" />
-  }
+  if (!hasMounted) return <div className="h-screen w-screen bg-background" />
 
   return <AudioPlayer />
 }
